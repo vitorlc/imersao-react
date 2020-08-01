1d543df2da8007337e0d4fa348b4b818bee13b00
vitorlc <vitor_cirqueira1@hotmail.com>
2020-08-01 12:29:27 -0300

    add menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Menu from './components/Menu'
 function App() {
   return (
     <div>
-      
+      <Menu></Menu>
     </div>
   );
 }
---
+++ src/components/Menu/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from '../../assets/img/Logo.png'
+import './Menu.css'
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="VitorFlix"></img>
+            </a>
+            <a className="ButtonLink" href="/">
+                Novo Vídeo
+            </a>
+        </nav>
+    )
+}
+
+export default Menu
